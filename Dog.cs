f44f15757e026f4579c10096cd96e0364473a4fb
Cajetan Ngede <134719269+cngede@users.noreply.github.com>
2024-05-10 04:32:18 -0400

    Add files via upload
    Task completed.

---
+++ Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Louisville_Exercise_2
+{
+    internal class Dog : Animal
+    {
+        public string ? Color { get; set; }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Code_Louisville_Exercise_2;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine() ?? "0");
+ 
+
+var recordList = new List<Dog>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    var myClass = new Dog();
+
+
+    Console.WriteLine("Enter the value for name");
+    myClass.Name = Console.ReadLine();
+
+    Console.WriteLine("Enter the value for color");
+    myClass.Color = Console.ReadLine();
+
+
+    recordList.Add(myClass);
+}
+foreach (var item in recordList)
+{
+    Console.WriteLine($"Dog Name: {item.Name} Dog Color:{item.Color}.");
+}
